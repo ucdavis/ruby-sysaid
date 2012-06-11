2e5e7f33fdd327a9180f1bbd69dbad6e66d511f4
Christopher Thielen <cthielen@gmail.com>
2012-06-11 12:42:15 -0700

    Implemented login

--- lib/sysaid.rb
+++ lib/sysaid.rb
@@ -1,6 +1,22 @@
 class SysAid
-  def self.hi
-    puts "Hello world!"
+  def initialize(account, username, password)
+    @account = account
+    @username = username
+    @password = password
+    
+    login
+  end
+  
+  private
+  def login
+    @service = SysaidApiService.new
+
+    # see SOAP wiredumps (for debugging)
+    @service.wiredump_dev = STDERR
+
+    # login
+    result = @service.login({:accountId => @account, :userName => @username, :password => @password})
+    @session_id = result.v_return
   end
 end
 
---
+++ lib/sysaid/ticket.rb
@@ -0,0 +1,3 @@
+class SysAid::Ticket
+  
+end
