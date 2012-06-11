da9daab86268f6660bf4166541ff8e7d4f2b7c5d
Christopher Thielen <cthielen@gmail.com>
2012-06-11 12:56:43 -0700

    Implemented find_by_ method beginnings and updated documentation to reflect future usage

--- README.md
+++ README.md
@@ -1,7 +1,20 @@
+Description
+===========
 ruby-sysaid is a Ruby-based wrapper for the SysAid SOAP API. It helps abstract
 the nonsense that is dealing with SOAP on Ruby and provides a clean interface
 for dealing with SysAid objects.
 
+Usage
+=====
+service = SysAid.new("account", "username", "password")
+ticket = service.find_by_id(45)
+ticket = service.find_by_responsibility("somebody")
+ticket.title = "Updated"
+ticket.save
+ticket.delete
+
+Additional
+==========
 Please file any bugs at https://github.com/cthielen/ruby-sysaid/issues.
 
 Written by Christopher Thielen for the University of California Davis.
@@ -9,4 +22,4 @@ Written by Christopher Thielen for the University of California Davis.
 Available under the MIT license.
 
 Version 0.1
-Last updated: Saturday, June 9, 2012
+Last updated: Monday, June 11, 2012
--- lib/sysaid.rb
+++ lib/sysaid.rb
@@ -7,6 +7,42 @@ class SysAid
     login
   end
   
+  # Implements find_by_* methods, e.g. find_by_id, find_by_responsibility, etc.
+  def method_missing(meth, *args, &block)
+    if meth.to_s =~ /^find_by_(.+)$/
+      run_find_by_method($1, *args, &block)
+    else
+      super
+    end
+  end
+
+  def run_find_by_method(attrs, *args, &block)
+    # Make an array of attribute names
+    attrs = attrs.split('_and_')
+
+    # #transpose will zip the two arrays together like so:
+    #   [[:a, :b, :c], [1, 2, 3]].transpose
+    #   # => [[:a, 1], [:b, 2], [:c, 3]]
+    attrs_with_args = [attrs, args].transpose
+
+    # Hash[] will take the passed associative array and turn it
+    # into a hash like so:
+    #   Hash[[[:a, 2], [:b, 4]]] # => { :a => 2, :b => 4 }
+    conditions = Hash[attrs_with_args]
+
+    sr = ApiServiceRequest.new
+    result = @service.executeSelectQuery({:sessionId => @session_id, :apiSysObj => sr, :condition => "#{conditions.keys[0]} = '#{conditions.values[0]}'"})
+    puts result
+  end
+  
+  def respond_to?(meth)
+    if meth.to_s =~ /^find_by_.*$/
+      true
+    else
+      super
+    end
+  end
+  
   private
   def login
     @service = SysaidApiService.new
